868702cb644813b311b8a138a3206ec2629b5a52
cchyung <chyungspice@gmail.com>
2020-04-10 19:54:42 -0600

    init commit!

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import gavin from './gavin.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={gavin} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
