05066796b6add9b4ed6cc85f5959b86c6e6502cb
Wilhelm Willie <wilhelmwillie@gmail.com>
2020-04-11 15:43:38 -0700

    initial work to setup frontend dev

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import gavin from './gavin.jpg'
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './pages/home';
+import Gavin from './pages/gavin';
+import TestApi from './pages/testApi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={gavin} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/gavin">
+          <Gavin />
+        </Route>
+
+        <Route path="/testApi">
+          <TestApi />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/hooks/useApi.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+// TODO: expand this hook to take on different params so we can handle non-GET requests
+function useApi(route) {
+
+  // Prefix route with production host
+  if (process.env.NODE_ENV === "production") {
+
+  }
+  const url = (process.env.NODE_ENV === "production") ? "https://sensehealth-backend.herokuapp.com/" + route : route;
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const request = await fetch(url);
+      const data = await request.json();
+
+      setResult(data);
+    }
+
+    getData();
+  }, [url]);
+
+  return result;
+}
+
+export default useApi;
